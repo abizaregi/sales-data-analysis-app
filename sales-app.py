2e38b45e70f6f2b0b7aa4ad9139ab3475673a65f
Abizar Egi <90840338+abizaregi@users.noreply.github.com>
2022-02-04 11:52:05 +0700

    Update sales-app.py

--- sales-app.py
+++ sales-app.py
@@ -66,11 +66,11 @@ def cityProduct(city):
 
 df_city = df_selected.groupby('Purchase Address City').sum()[['Quantity Ordered', 'Price Total']].sort_values(by='Price Total', ascending=False)
 df_city['Top 3 Product'] = list(map(cityProduct, df_city.index))
-st.dataframe(df_city.head())
+st.dataframe(df_city)
 
-plot_price = df_selected.groupby('Purchase Address City').sum()['Price Total'].sort_values(ascending=False)
+plot_price = df_selected.groupby('Purchase Address City').sum()['Price Total']
 st.bar_chart(plot_price)
-plot_qty = df_selected.groupby('Purchase Address City').sum()['Quantity Ordered'].sort_values(ascending=False)
+plot_qty = df_selected.groupby('Purchase Address City').sum()['Quantity Ordered']
 st.bar_chart(plot_qty)
 
 Qty = df_selected.groupby('Product').sum()['Quantity Ordered'].sort_values(ascending=False).head()
